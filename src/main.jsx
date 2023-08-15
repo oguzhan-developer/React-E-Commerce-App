78811371d3f869adbb66f741b894dc4e78cc6339
Oğuzhan YALÇIN <60703483+oguzhan-developer@users.noreply.github.com>
2023-08-15 20:07:36 +0000

    Update 5 files

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./redux/store.jsx";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev/index.js";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.jsx";
 
-document.title = import.meta.env.VITE_SITE_NAME
+document.title = import.meta.env.VITE_SITE_NAME;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <DevSupport ComponentPreviews={ComponentPreviews}
-                        useInitialHook={useInitial}
-            >
-                <App/>
-            </DevSupport>
-        </Provider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
